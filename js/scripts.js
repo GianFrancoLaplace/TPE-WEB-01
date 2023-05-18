ac83f01e5b59baaf55b79628da52c825e0cd88cf
GianFrancoLaplace <laplacegian@gmail.com>
2023-05-18 11:58:58 -0300

    Validación del form Gian, ajustes al footer

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,11 @@
 const buttonMode = document.getElementById("btn_mode");
 const mainColor = [...document.querySelectorAll(".priColor")];
-const images = document.getElementById('images');
-const btn_form = document.getElementById("button-form");
-const btnPrueba = document.getElementById("btnPrueba");
 
 if(buttonMode){
-buttonMode.addEventListener("click", () =>{
-    changePrimaryColor();
-})
+    buttonMode.addEventListener("click", () =>{
+        changePrimaryColor();
+    })
 }
-console.log(mainColor);
-
 
 const changePrimaryColor = () =>{
     if(mainColor[0].classList.contains("secColor")){
@@ -20,19 +15,60 @@ const changePrimaryColor = () =>{
     } 
     else{
         buttonMode.textContent = "Bright Mode";
-        mainColor.map(i=>i.classList.add('secColor'));
+        mainColor.map(i=>i.classList.add("secColor"));
         // mainColor.classList.add('secColor');
     }
 }
 
-if(btn_form)
-btn_form.addEventListener("click", (e) =>{
-    console.log("Messi")
-    e.preventDefault();
+const form = document.getElementById("form")
+const name = document.getElementById("input-name")
+const email = document.getElementById("input-email")
+const subject = document.getElementById("input-subject")
+const msg = document.getElementById("input-msg")
+
+const formIsValid = {
+    name: false,
+    email: false,
+    subject: true,
+    msg: false
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    validateForm()
 })
 
-if(btnPrueba)
-btnPrueba.addEventListener("click", () =>{
-    console.log("Click");
+name.addEventListener("change", (e) => {
+    if(e.target.value.trim().length > 0) formIsValid.name = true
 })
 
+email.addEventListener("change", (e) => {
+    if(e.target.value.trim().length > 0) formIsValid.email = true
+})
+
+msg.addEventListener("change", (e) => {
+    if(e.target.value.trim().length > 0)
+    formIsValid.msg = true 
+})
+
+const validateForm = () => {
+    const formValues = Object.values(formIsValid)
+    const valid = formValues.findIndex(value => value == false)
+    if(valid == -1) alert("Form valid")
+    else alert('Form invalid')
+}
+
+// let form = document.getElementById("form");
+// form.addEventListener("submit", agregar)
+// let btn_form = document.getElementById("button-form");
+
+// function agregar(e){
+//     e.preventDefault();
+
+//     let formData = new formData(form);
+    
+//     let name = formData.get("user_name")
+//     let email = formData.get("user_email")
+//     let subject= formData.get("user_subject")
+//     let msg = formData.get("user_message")
+// }
